6d36d9d6de3952df28f000fad429143d0ce03648
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-11-20 16:18:46 -0500

    added fonts and fixed some alignment

--- assets/js/project-populator.js
+++ assets/js/project-populator.js
@@ -15,7 +15,9 @@ function main() {
         - populate the project 
     */
     projectHolder = document.getElementById("project-holder"); // JQuery?
-    projectHolder.innerHTML += populate("4x3 Test", "./assets/images/test4_3.png", "This is a sample description, to be swapped out later", "The Internet!", "Tomorrow");
+    for (let i = 0; i < 10; i++) {
+        projectHolder.innerHTML += populate("Project Test", "./assets/images/test16_9.png", "This is a sample description, to be swapped out later", "The Internet!", "Tomorrow");
+    }
 }
 
 function populate(projectTitle, imageLink, projectDescription, platform, date) {
@@ -25,8 +27,10 @@ function populate(projectTitle, imageLink, projectDescription, platform, date) {
                     ${projectTitle}
                 </project-title>
                 <br>
-                <img src="${imageLink}">
+                <div class="project-image" style="background-image: url(${imageLink});">
                 <br>
+            </div>
+            <div class="project-middle">
                 <description>
                     ${projectDescription}
                 </description>
