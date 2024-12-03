8fcf6e95e7e5976eb9922907af844be73f83c7e4
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-12-03 12:27:52 -0500

    fleshed out responsivity

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,6 +1,7 @@
 // GLOBAL VARIABLES AND CONSTANTS
 
-
+var TRE;
+var windowStatus;
 
 // EVENT BINDINGS
 
@@ -9,5 +10,34 @@ document.addEventListener("DOMContentLoaded", beginResponsiveDesign);
 // FUNCTION DECLARATIONS
 
 function beginResponsiveDesign() {
-    // put stuff here later
+    TRE = document.getElementById("tre");
+
+    switchViewsIfAppropriate();
+    window.addEventListener("resize", switchViewsIfAppropriate);
+}
+
+function desktopView() {
+    console.log("[responsive.js] switching to desktop view");
+    TRE.innerHTML = "TheRedEncryption";
+}
+
+function mobileView() {
+    console.log("[responsive.js] switching to mobile view");
+    TRE.innerHTML = "T.R.E.";
+}
+
+function switchViewsIfAppropriate() {
+    let vw = window.innerWidth;
+    let vh = window.innerHeight;
+
+    let aspectRatio = vw / vh;
+
+    if (windowStatus != "mobile" && aspectRatio < 1.0) {
+        windowStatus = "mobile";
+        mobileView();
+    } else if (windowStatus != "desktop" && aspectRatio >= 1.0) {
+        windowStatus = "desktop";
+        desktopView();
+    }
+
 }
