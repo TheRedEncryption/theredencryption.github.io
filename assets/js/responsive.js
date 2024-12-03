0a6c9e652aca7d8397c277f08499cd10e5f34d8a
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-12-03 11:54:47 -0500

    fixed landing page small text as well as began responsive.js

---
+++ assets/js/responsive.js
@@ -0,0 +1,13 @@
+// GLOBAL VARIABLES AND CONSTANTS
+
+
+
+// EVENT BINDINGS
+
+document.addEventListener("DOMContentLoaded", beginResponsiveDesign);
+
+// FUNCTION DECLARATIONS
+
+function beginResponsiveDesign() {
+    // put stuff here later
+}
